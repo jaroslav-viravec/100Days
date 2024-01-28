cd94606f05b1e4437b543b8fafa4395438e2d377
Jaroslav Viravec <jaroslav.viravec@sap.com>
2024-01-28 19:16:01 +0100

    spirograph added

---
+++ spirograph/spirograph.py
@@ -0,0 +1,25 @@
+import turtle as t
+import random
+
+turtle = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    color = (r, g, b)
+    return color
+
+turtle.speed("fastest")
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        turtle.color(random_color())
+        turtle.circle(100)
+        turtle.setheading(turtle.heading() + 10)
+
+draw_spirograph(10)        
+    
+# remain the window and waint for click
+screen = t.Screen()
+screen.exitonclick()
