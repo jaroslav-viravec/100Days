4780a7b091b6957fef53ae86d7a70437d0c683e5
Jaroslav Viravec <jaroslav.viravec@sap.com>
2024-02-04 15:32:35 +0100

    turtle-cross-to-road added

---
+++ turtle-cross-to-road/main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle cross a road")
+screen.tracer(0)
+
+player = Player()
+carmanager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+# move turtle up/down/right/left
+screen.onkey(player.go_up,"Up")
+screen.onkey(player.go_down,"Down")
+screen.onkey(player.go_right,"Right")
+screen.onkey(player.go_left,"Left")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    carmanager.create_car()
+    carmanager.more_cars()
+
+    # detect collision with car
+    for car in carmanager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    # detect succesful crossing
+    if player.is_at_finish_line():
+        player.go_to_start()
+        carmanager.level_up()
+        scoreboard.increase_level()
+screen.exitonclick()
